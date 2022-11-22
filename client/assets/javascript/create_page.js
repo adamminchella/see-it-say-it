0e67dec88848d562d720cdff63b46226a3f5f0dc
Polly <pollyannafenne@gmail.com>
2022-11-22 18:05:56 +0000

    Script updates

--- client/assets/javascript/create_page.js
+++ client/assets/javascript/create_page.js
@@ -1,26 +1,66 @@
-
-
+let button = document.getElementsByClassName("button");
 const form = document.querySelector("form");
+
+let labels = []
+
+Array.from(button).forEach(button => {
+   button.addEventListener("click", (e) => {
+      e.preventDefault()
+      const clickedButton = button.value;
+      console.log(labels);
+
+      if (labels.length <= 3) {
+         console.log("test")
+         for (let value of labels) {
+            if (value == clickedButton) {
+               console.log(labels.indexOf(value))
+               labels.splice(labels.indexOf(value), 1)
+               console.log(labels)
+            }
+            else {
+               labels.push(clickedButton)
+               console.log(clickedButton)
+            }
+         }
+      }
+      else {
+         console.log("Please select max 3 labels")
+      }
+   })
+})
+
 
 
 form.addEventListener("submit", function(e) {
    e.preventDefault();
 
-   const formData = new FormData(form);
-   const data = Object.fromEntries(formData);
+   let titleText = document.getElementById("myTitle").value;
+   let descriptionText = document.getElementById("myDescription").value;
+   let locationText = document.getElementById("myLocation").value;
+
+   let dataToSend = {
+      title: titleText,
+      description: descriptionText,
+      location: locationText,
+      labels: labels
+   }
+
+   console.log(dataToSend)
 
    fetch("http://localhost:3000/api/posts", {
       method: "POST",
       headers: {
          "Content-Type": "application/json"
       },
-      body: JSON.stringify(data)
+      body: JSON.stringify(dataToSend)
    })
-      .then(response => response.json()
-      .then(data => console.log(data)))
-      .catch(error => console.error());
+      .then(response => response.json())
+      // .then(data => { 
+      //    return (
+      //    window.location.href = `post.html?${data.postId}`)})
 
-      let postID = data.postID
-      window.location.href = ``
+      .catch(error => console.log(error));
 });
+
+
 

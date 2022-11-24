7618cc377692a7c08cf8e7ab5685a6e8d95c062c
“liambrockpy” <liambrock72@gmail.com>
2022-11-24 12:43:49 +0000

    Remove bundler

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,7 +1,5 @@
-const landingScript = () => {
-    if (decodeURIComponent(document.cookie)) {
-        window.location.href = 'html/home.html'
-    } else {
-        document.cookie = 'visited=true'
-    }
+if (decodeURIComponent(document.cookie)) {
+    window.location.href = 'html/home.html'
+} else {
+    document.cookie = 'visited=true'
 }
